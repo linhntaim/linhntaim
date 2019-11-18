978a7e27298034eef1771663df0e5b9bc9bfef82
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2019-11-19 00:18:49 +0700

    play music background pos

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -17,8 +17,20 @@
 </template>
 
 <script>
+    import {APP_URL} from '../../app/config'
+
     export default {
         name: 'Home',
+        data() {
+            return {
+                audio: null,
+            }
+        },
+        mounted() {
+            this.audio = new Audio(APP_URL + '/sites/sounds/if_you_want_love.mp3')
+            this.audio.loop = true
+            this.audio.play()
+        },
     }
 </script>
 
