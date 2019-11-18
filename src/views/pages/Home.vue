6b23fcbd7c6014a310ace6453e8a82d6ebfca785
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2019-11-18 23:46:22 +0700

    home

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,6 +1,19 @@
 <template lang="pug">
-    #home.fullscreen
+    #home
         h1 LINHNTAIM
+        blockquote
+        nav
+            .nav-item
+                a(href="https://hey.linhntaim.com" target="_blank")
+                    span hey
+            .nav-item-divider
+            .nav-item
+                a(href="https://resume.linhntaim.com" target="_blank")
+                    span my resume
+            .nav-item
+                a(href="https://pinkspirit.linhntaim.com" target="_blank")
+                    span pinkspirit
+            .nav-item-divider
 </template>
 
 <script>
